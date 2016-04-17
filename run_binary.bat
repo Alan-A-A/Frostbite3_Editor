c1fce1eb798a0cc425187bf2356a6a0a60f9cc74
Alan A. A. <alan.cpp@outlook.com>
2016-04-17 16:18:22 +0200

    Converted to maven project.

--- run_binary.bat
+++ run_binary.bat
@@ -6,6 +6,6 @@ echo JavaFX does only works on Java 8+
 echo.
 echo Please type in any additional arguments. [ENTER]
 set /p commandLineArgs=
-java -Xmx2048m -cp binary_build.jar -Djava.library.path=lib\native\windows tk.greydynamics.Game.Core %commandLineArgs%
+java -Xmx2048m -cp FrostBite3Editor-0.4.0-SNAPSHOT.jar -Djava.library.path=lib\native\windows tk.greydynamics.Game.Core %commandLineArgs%
 echo Press any key to EXIT!
 pause >nul
